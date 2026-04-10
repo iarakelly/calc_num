3731cc0c8e1f32fb47d157887d48a311193718b7
iarakelly <iarakelly.ba@gmail.com>
2026-04-10 18:15:57 -0300

    exerc zeros

--- zeros-of-functions/question9.py
+++ zeros-of-functions/question9.py
@@ -1,3 +1,4 @@
+import numpy as np
 
  #𝑓(𝑥) = 𝑥4 − 2.36343𝑥3 − 18.1163𝑥2 + 20.7595𝑥 + 58.8273
 def f(x):
@@ -10,7 +11,7 @@ def bissection_method(a, b, precision):
     #step one: check the sign of f(a) and f(b)
     # Base case: if f(a) and f(b) have the same sign, then there is no root in the interval [a, b]
     if abs(b - a) < precision:
-        print("The root is approximately: ", (a + b) / 2)
+        #print("The root is approximately: ", (a + b) / 2)
         return (a + b) / 2
 
     m = (a+b)/2
@@ -18,11 +19,48 @@ def bissection_method(a, b, precision):
     f_b = f(b)
     f_m = f(m) 
 
-    if f_a *f_m < 0:
-        a = m
-        return bissection_method(a, b, precision)
-    else:
+    if f_a *f_m < 0: #[a,m]
         b = m
         return bissection_method(a, b, precision)
-precision = 0.5
-bissection_method(-40, 34, precision)
+    else:
+        a = m
+        return bissection_method(a, b, precision)
+    
+precision = 0.0001
+bissection_method(-2, 3, precision) #obs: if the inteval is too large, the method can not working 
+
+#2. newton method
+
+def df(x):
+    return 4*x**3 - 7.09029*x**2 - 36.2326*x + 20.7595
+
+    #transform in recurssion function after
+def newthon_method(x, precision):
+
+    while True:
+    
+        next_x = x - (f(x)/ df(f(x))) # calc next x
+
+        #stopped if
+        if abs(next_x -x) < precision:
+            print("The root is approximately: ", next_x) 
+            break
+
+        x = next_x
+
+newthon_method(2, precision)
+
+#3. secant method
+
+def secant_method(a, b, precision):
+
+    while True:
+    
+        next_x = a - (f(a)/((f(a))-f(b))/(a-b)) # calc next x
+
+        #stopped if
+        if abs(next_x -b) < precision:
+            print("The root is approximately: ", next_x) 
+            break
+
+        a, b = b, next_x
